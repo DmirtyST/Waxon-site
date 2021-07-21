bfffb58bcfcc0c9761259f6adb2467fbd19986ac
DmirtyST <76765743+DmirtyST@users.noreply.github.com>
2021-07-21 21:53:28 +0300

    Waxon
    Waxon

---
+++ js/app.js
@@ -0,0 +1,237 @@
+
+//=================
+//Tabs
+let tabs = document.querySelectorAll("._tabs");
+for (let index = 0; index < tabs.length; index++) {
+	let tab = tabs[index];
+	let tabs_items = tab.querySelectorAll("._tabs-item");
+	let tabs_blocks = tab.querySelectorAll("._tabs-block");
+	for (let index = 0; index < tabs_items.length; index++) {
+		let tabs_item = tabs_items[index];
+		tabs_item.addEventListener("click", function (e) {
+			for (let index = 0; index < tabs_items.length; index++) {
+				let tabs_item = tabs_items[index];
+				tabs_item.classList.remove('_active');
+				tabs_blocks[index].classList.remove('_active');
+			}
+			tabs_item.classList.add('_active');
+			tabs_blocks[index].classList.add('_active');
+			e.preventDefault();
+		});
+	}
+}
+//=================
+
+//=================
+//Gallery
+let gallery = document.querySelectorAll('._gallery');
+if (gallery) {
+	gallery_init();
+}
+function gallery_init() {
+	for (let index = 0; index < gallery.length; index++) {
+		const el = gallery[index];
+		lightGallery(el, {
+			counter: false,
+			selector: 'a',
+			download: false
+		});
+	}
+}
+//=================
+/
+//Popups
+let popup_link = document.querySelectorAll('._popup-link');
+let popups = document.querySelectorAll('.popup');
+for (let index = 0; index < popup_link.length; index++) {
+	const el = popup_link[index];
+	el.addEventListener('click', function (e) {
+		if (unlock) {
+			let item = el.getAttribute('href').replace('#', '');
+			let video = el.getAttribute('data-video');
+			popup_open(item, video);
+		}
+		e.preventDefault();
+	})
+}
+for (let index = 0; index < popups.length; index++) {
+	const popup = popups[index];
+	popup.addEventListener("click", function (e) {
+		if (!e.target.closest('.popup__body')) {
+			popup_close(e.target.closest('.popup'));
+		}
+	});
+}
+function popup_open(item, video = '') {
+	let activePopup = document.querySelectorAll('.popup._active');
+	if (activePopup.length > 0) {
+		popup_close('', false);
+	}
+	let curent_popup = document.querySelector('.popup_' + item);
+	if (curent_popup && unlock) {
+		if (video != '' && video != null) {
+			let popup_video = document.querySelector('.popup_video');
+			popup_video.querySelector('.popup__video').innerHTML = '<iframe src="https://www.youtube.com/embed/' + video + '?autoplay=1"  allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+		}
+		if (!document.querySelector('.menu__body._active')) {
+			body_lock_add(500);
+		}
+		curent_popup.classList.add('_active');
+		history.pushState('', '', '#' + item);
+	}
+}
+function popup_close(item, bodyUnlock = true) {
+	if (unlock) {
+		if (!item) {
+			for (let index = 0; index < popups.length; index++) {
+				const popup = popups[index];
+				let video = popup.querySelector('.popup__video');
+				if (video) {
+					video.innerHTML = '';
+				}
+				popup.classList.remove('_active');
+			}
+		} else {
+			let video = item.querySelector('.popup__video');
+			if (video) {
+				video.innerHTML = '';
+			}
+			item.classList.remove('_active');
+		}
+		if (!document.querySelector('.menu__body._active') && bodyUnlock) {
+			body_lock_remove(500);
+		}
+		history.pushState('', '', window.location.href.split('#')[0]);
+	}
+}
+let popup_close_icon = document.querySelectorAll('.popup__close,._popup-close');
+if (popup_close_icon) {
+	for (let index = 0; index < popup_close_icon.length; index++) {
+		const el = popup_close_icon[index];
+		el.addEventListener('click', function () {
+			popup_close(el.closest('.popup'));
+		})
+	}
+}
+document.addEventListener('keydown', function (e) {
+	if (e.code === 'Escape') {
+		popup_close();
+	}
+});
+
+//=================
+
+const menuIcon = document.querySelector 
+(".burger__menu");
+const nav = document.querySelector ('nav');
+
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+});
+
+$(document).ready(function() {
+
+
+    $('.slider-home').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: 'ease',
+        infinite: false,
+        dots:true,
+    });
+    
+
+   
+    
+    
+    
+    });
+
+
+    
+
+
+
+ $(document).ready(function() {
+
+
+ $('.Recent-Posts__slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      variableWidth: true,
+      centerMode: true,
+      easing: 'ease',
+      speed: 1000,
+      responsive: [
+         {
+         breakpoint: 768,
+         settings: {
+         autoplay:true,
+         pauseOnFocus:true,
+         autoplaySpeed: 800,
+         pauseOnHover:true,
+         centerMode: false,
+         centerPadding: '40px',
+         slidesToShow: 1
+                  }
+         }
+               
+                
+         ]
+                
+            });
+            
+               
+            
+            });
+
+
+            
+
+            
+$('.spoller').click(function (event) {
+            if ($(window).width() <1140) {
+                
+                $(this).toggleClass('active').next().slideToggle(600);
+            }
+        });
+        
+
+//number counter...-----------------------------------------------------------
+
+
+        $.fn.jQuerySimpleCounter = function( options ) {
+            var settings = $.extend({
+                start:  0,
+                end:    100,
+                easing: 'swing',
+                duration: 600,
+                complete: ''
+            }, options );
+    
+            var thisElement = $(this);
+    
+            $({count: settings.start}).animate({count: settings.end}, {
+                duration: settings.duration,
+                easing: settings.easing,
+                step: function() {
+                    var mathCount = Math.ceil(this.count);
+                    thisElement.text(mathCount);
+                },
+                complete: settings.complete
+            });
+        };
+    
+      
+    
+    $('#number1').jQuerySimpleCounter({end: 3587,duration: 3000});
+    $('#number2').jQuerySimpleCounter({end: 207,duration: 3000});
+    $('#number3').jQuerySimpleCounter({end: 2500,duration: 3000});
+    $('#number4').jQuerySimpleCounter({end: 873,duration: 3000});
+    $('#number5').jQuerySimpleCounter({end: 900,duration: 3000});
+    
+      
+
+
